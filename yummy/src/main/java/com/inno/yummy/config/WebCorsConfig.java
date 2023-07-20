c57576b85418758f0569d28f18ccb7496da8ecc1
YiHwanRyu <yihwandev@gmail.com>
2023-07-20 18:42:42 +0900

    [UPDATE] 스프링부트 버전에 따른 메서드 수정

--- yummy/src/main/java/com/inno/yummy/config/WebCorsConfig.java
+++ yummy/src/main/java/com/inno/yummy/config/WebCorsConfig.java
@@ -9,8 +9,10 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001") // 허용할 주소
+                //.allowedOrigins("http://localhost:3000", "http://localhost:3001") // 허용할 주소
                 //.allowedOrigins 여러개 사용하면 적용 안됨. 한개에 여러 주소 입력필요.
+                // allowedOriginPatterns -> allowCredentials와 사용가능
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:3001")
                 .allowedMethods("*") // 모든 HTTP 메서드에 대해 허용
                 .allowCredentials(true) // 토큰관련 설정
                 .exposedHeaders("Authorization"); // 허용할 헤더 지정
