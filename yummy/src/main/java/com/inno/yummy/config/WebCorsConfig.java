e3554b96c6566a3f2d132ac2857ccf1cc9356550
YiHwanRyu <yihwandev@gmail.com>
2023-07-20 14:58:41 +0900

    [UPDATE] Cors설정 중 토큰 허용, 허용할 헤더 지정

--- yummy/src/main/java/com/inno/yummy/config/WebCorsConfig.java
+++ yummy/src/main/java/com/inno/yummy/config/WebCorsConfig.java
@@ -9,7 +9,10 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8083") // 허용할 주소
-                .allowedMethods("*"); // 모든 HTTP 메서드에 대해 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001") // 허용할 주소
+                //.allowedOrigins 여러개 사용하면 적용 안됨. 한개에 여러 주소 입력필요.
+                .allowedMethods("*") // 모든 HTTP 메서드에 대해 허용
+                .allowCredentials(true) // 토큰관련 설정
+                .exposedHeaders("Authorization"); // 허용할 헤더 지정
     }
 }
